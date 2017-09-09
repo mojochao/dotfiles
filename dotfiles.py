0937cfb652f326abf8e1a60616360fa5e693d2e9
Allen Gooch <allen.gooch@gmail.com>
2017-09-08 19:19:13 -0500

    Small improvements.

--- dotfiles.py
+++ dotfiles.py
@@ -1,40 +1,41 @@
 #!/usr/bin/env python
 from __future__ import print_function
-import argparse
-import os
+from argparse import ArgumentParser
+from os import makedirs, path, symlink, unlink
 
 
-SRC_DIR = os.path.abspath(os.path.join(os.path.dirname(__file__), 'src'))
+MANIFEST_FILE = 'manifest.in'
+SRC_DIR = path.abspath(path.join(path.dirname(__file__), 'src'))
 
 
 def install(item):
-    src = os.path.join(SRC_DIR, item)
-    dst = os.path.expanduser('~/{}'.format(item))
-    if not os.path.exists(dst):
-        containing_dir = os.path.dirname(dst)
-        if not os.path.exists(containing_dir):
-            os.makedirs(containing_dir)
-        os.symlink(src, dst)
+    src = path.join(SRC_DIR, item)
+    dst = path.expanduser('~/{}'.format(item))
+    if not path.exists(dst):
+        containing_dir = path.dirname(dst)
+        if not path.exists(containing_dir):
+            makedirs(containing_dir)
+        symlink(src, dst)
         return 'installed {}'.format(dst)
     else:
         return '{} found and skipped'.format(dst)
 
 
 def uninstall(item):
-    dst = os.path.expanduser('~/{}'.format(item))
-    if os.path.islink(dst):
-        os.unlink(dst)
+    dst = path.expanduser('~/{}'.format(item))
+    if path.islink(dst):
+        unlink(dst)
         return 'uninstalled {}'.format(dst)
     else:
         return '{} not found and skipped'.format(dst)
 
 
 def main():
-    parser = argparse.ArgumentParser(description='manage dotfiles in home directory')
+    parser = ArgumentParser(description='manage dotfiles in home directory')
     parser.add_argument('action', choices=['install', 'uninstall'], help='desired action')
     args = parser.parse_args()
 
-    with open('manifest.in') as infile:
+    with open(MANIFEST_FILE) as infile:
         dotfiles = [i for i in infile.read().split('\n') if i != '']
 
     results = map(globals()[args.action], dotfiles)
